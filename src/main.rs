1fac50c33b31c13cc9451ce27dda8dcbab8da4bb
YamadaKigo <1652928719@qq.com>
2024-09-14 04:39:11 +0800

    第3次提交

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use std::io;
-
+use std::{cmp::Ordering, io};
+use rand::Rng; // trait
+ 
 fn main() {
     println!("Guessing Numbers!");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-    .expect("Cannot read line.");
-
-    println!("Your guessed number is：{}", guess);
+    println!("Generating pseudo random numbers.");
+    let secret_number: u32 = rand::thread_rng().gen_range(1.. 101);
+    //println!("The random number is:{}", secret_number);
     
-    //test
+    loop {
+        println!("Please guess a number!");
+        let mut guess: String = String::new();
+    
+        io::stdin().read_line(&mut guess)
+        .expect("Cannot read line.");
+    
+        let guess: u32= match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("Your guessed number is:{}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            },
+        };
+    }
 }
