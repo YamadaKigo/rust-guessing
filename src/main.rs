3830650f272249708491aaa43bb9c65259ada3f8
YamadaKigo <1652928719@qq.com>
2024-09-14 00:34:02 +0800

    第一次提交

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Guessing Numbers!");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    .expect("Cannot read line.");
+
+    println!("Your guessed number is：{}", guess);
+    
+}
